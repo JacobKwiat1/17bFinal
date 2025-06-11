d0c3a792a2c1d0e47d7099e53da5ecb770b1c19d
Jacob <jacobkwiat@gmail.com>
2025-06-10 22:12:05 -0700

    Add files via upload

--- s/admin.php
+++ s/admin.php
@@ -2,8 +2,7 @@
 $pageTitle = "Stats";
 include( "includes/header.php");
 require_once("connect.php");
-
-?>
+echo "
 <tr>
 <table border='0' width='75%' cellspacing='3' cellpadding='3' align='center'>
 <tr>
@@ -11,22 +10,21 @@ require_once("connect.php");
 <td align='center' width='30%'><b>Email</b></td>
 <td align='center' width='25%'><b>totalTime</b></td>
 <td align='right' width='25%'><b>Admin</b></td>
-</tr>
-<?php
+</tr>";
 
 $req = "SELECT ID, username, email, totalTime, admin FROM user";
 $result = $connection->query($req);
     foreach( $result as $row ){
-        ?>
+        echo "
             <tr>
-            <td align='left'><?= $row['ID']?></td>
-            <td align='left'><?= $row['username'] ?></td>
-            <td align='center'><?= $row['email'] ?></td>
-            <td align='center'><?= $row['totalTime'] ?></td>
-            <td align='right'><?= $row['admin'] ?></td>
-            <td align='right'><form method='POST' action='admin.php'><input type='submit' name='<?= $row['ID'] ?>' value='DELETE'></form></td> 
-        </tr>;
-<?php
+            <td align='left'>$row[ID]</td>
+            <td align='left'>$row[username] </td>
+            <td align='center'>$row[email]</td>
+            <td align='center'>$row[totalTime]</td>
+            <td align='right'>$row[admin]</td>
+            <td align='right'><form method='POST' action='admin.php'><input type='submit' name=$row[ID] value='DELETE'></form></td> 
+        </tr>
+        </table>";
     }
 include('includes/footer.php');
     if($_SERVER["REQUEST_METHOD"] === "POST") {
